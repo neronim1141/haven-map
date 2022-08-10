94bb59e4810d7abdc939437ccd2f092bdf296c7f
DESKTOP-L8P1VHU\neron <neronim66@gmail.com>
2022-08-10 14:58:49 +0200

    moved updateMarker main function to features

---
+++ features/map/api/markerUpdate.ts
@@ -0,0 +1,39 @@
+import { prisma } from "lib/prisma";
+
+export type MarkersRequest = {
+  id: number;
+  image: string;
+  x: number;
+  y: number;
+  gridID: string;
+  name: string;
+  type: string;
+}[];
+
+export const markerUpdate = async (markers: MarkersRequest) => {
+  for (let marker of markers) {
+    const { gridID, ...data } = marker;
+    const grid = await prisma.grid.findUnique({ where: { id: gridID } });
+    if (!grid) continue;
+
+    if (data.type === "player") continue;
+    if (data.image == "") {
+      data.image = "gfx/terobjs/mm/custom";
+      data.type = "custom";
+    }
+
+    await prisma.marker.upsert({
+      where: {
+        id: data.id,
+      },
+      update: {
+        ...data,
+        gridId: gridID,
+      },
+      create: {
+        ...data,
+        gridId: gridID,
+      },
+    });
+  }
+};
--- pages/api/client/markerUpdate.tsx
+++ pages/api/client/markerUpdate.tsx
@@ -3,46 +3,12 @@ import { createRouter } from "next-connect";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "lib/prisma";
 import * as logger from "lib/logger";
+import { MarkersRequest, markerUpdate } from "features/map/api/markerUpdate";
 const router = createRouter<NextApiRequest, NextApiResponse>();
-
-type MarkersRequest = {
-  id: number;
-  image: string;
-  x: number;
-  y: number;
-  gridID: string;
-  name: string;
-  type: string;
-}[];
 
 router.post(async (req, res) => {
   logger.log("markerUpdate");
-  const markers = req.body as MarkersRequest;
-  for (let marker of markers) {
-    const { gridID, ...data } = marker;
-    const grid = await prisma.grid.findUnique({ where: { id: gridID } });
-    if (!grid) continue;
-
-    if (data.type === "player") continue;
-    if (data.image == "") {
-      data.image = "gfx/terobjs/mm/custom";
-      data.type = "custom";
-    }
-
-    await prisma.marker.upsert({
-      where: {
-        id: data.id,
-      },
-      update: {
-        ...data,
-        gridId: gridID,
-      },
-      create: {
-        ...data,
-        gridId: gridID,
-      },
-    });
-  }
+  await markerUpdate(req.body as MarkersRequest);
 
   res.end();
 });
