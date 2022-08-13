faf40718c6b5c6195dbaa0b06e3df2aa03cc9810
DESKTOP-L8P1VHU\neron <neronim66@gmail.com>
2022-08-13 12:33:23 +0200

    removed tileUrl

--- features/map/api/utils/saveTile.ts
+++ features/map/api/utils/saveTile.ts
@@ -10,10 +10,6 @@ export const saveTile = async (
   file: Buffer,
   gridId?: string
 ) => {
-  let dir = path.join("public", "grids", mapId.toString(), z.toString());
-  await fs.mkdir(dir, { recursive: true });
-  dir = path.join(dir, `${x}_${y}.webp`);
-  await fs.writeFile(dir, file);
   let tile = await prisma.tile.findFirst({
     where: {
       mapId,
@@ -29,13 +25,7 @@ export const saveTile = async (
       },
       data: {
         tileData: file,
-        tileUrl: path.join(
-          "/",
-          "grids",
-          tile.mapId.toString(),
-          tile.z.toString(),
-          `${tile.x}_${tile.y}.webp`
-        ),
+
         lastUpdated: Date.now().toString(),
       },
     });
@@ -47,13 +37,6 @@ export const saveTile = async (
         y,
         z,
         tileData: file,
-        tileUrl: path.join(
-          "/",
-          "grids",
-          mapId.toString(),
-          z.toString(),
-          `${x}_${y}.webp`
-        ),
         lastUpdated: Date.now().toString(),
         gridId,
       },
