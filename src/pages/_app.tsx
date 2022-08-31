f779f8db3c6d9f78a3ed583e8b895dc4ae763ca0
DESKTOP-L8P1VHU\neron <neronim66@gmail.com>
2022-08-31 16:11:41 +0200

    env var as url for trpc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -34,7 +34,8 @@ export default withTRPC<AppRouter>({
      * If you want to use SSR, you need to use the server's full URL
      * @link https://trpc.io/docs/ssr
      */
-    const url = "http://localhost:3000/api/trpc";
+    const url =
+      (process.env.NEXT_PUBLIC_PREFIX ?? "http://localhost:3000") + "/api/trpc";
     return {
       url,
       transformer: superjson,
