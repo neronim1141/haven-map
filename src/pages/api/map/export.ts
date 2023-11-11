f2f7b81aba4e05b3bac3bd3705ef11466f7a9a06
Kacper Kruczek <kacper.m.kruczek@email.com>
2023-11-12 00:46:54 +0100

    fix prisma error on export by particioning

--- src/pages/api/map/export.ts
+++ src/pages/api/map/export.ts
@@ -1,52 +1,87 @@
 import { createRouter } from "next-connect";
 import { prisma } from "utils/prisma";
-import { logger } from "utils/logger";
 import type { NextApiRequest, NextApiResponse } from "next";
-import jszip, { JSZipObject } from "jszip";
+import jszip from "jszip";
 import { parallel } from "radash/dist/async";
-import { Marker } from "@prisma/client";
 
 const router = createRouter<NextApiRequest, NextApiResponse>();
+const partSize = 1000;
+
+const getGridsByIteration = async (
+  iteration: number,
+  mapId: number,
+  zip: jszip
+) => {
+  const grids = await prisma.grid.findMany({
+    where: {
+      mapId,
+    },
+    select: {
+      id: true,
+      x: true,
+      y: true,
+      mapId: true,
+      tileData: true,
+    },
+    take: partSize,
+    skip: iteration * partSize,
+  });
+  let markersData: string[] = [];
+
+  for (let grid of grids) {
+    if (grid.tileData) {
+      zip.file(`${mapId}/${grid.id}_${grid.x}_${grid.y}.png`, grid.tileData);
+    }
+
+    const markers = await prisma.marker.findMany({
+      where: {
+        gridId: grid.id,
+      },
+    });
+    for (let marker of markers) {
+      markersData.push(
+        `${marker.id}:${marker.name}:${marker.image}:${marker.gridId}:${
+          marker.x
+        }:${marker.y}:${marker.hidden ? 1 : 0}`
+      );
+    }
+  }
+  return markersData;
+};
 
 router.get(async (req, res) => {
   const zip = new jszip();
   const maps = await prisma.map.findMany();
-  zip.file(`maps.json`, JSON.stringify(maps));
+
+  zip.file(
+    `maps.csv`,
+    maps
+      .map(
+        (map) =>
+          `${map.id}:${map.name}:${map.hidden ? 1 : 0}:${
+            map.priority ? 1 : 0
+          }:${map.winterUpdate ? 1 : 0}`
+      )
+      .join(";\n")
+  );
 
   for (let map of maps) {
-    const grids = await prisma.grid.findMany({
+    let count = await prisma.grid.count({
       where: {
         mapId: map.id,
       },
-      select: {
-        id: true,
-        x: true,
-        y: true,
-        mapId: true,
-        tileData: true,
-      },
     });
-    let markers: Marker[] = [];
-    await parallel(5, grids, async (grid) => {
-      if (grid.tileData) {
-        zip.file(`${map.id}/${grid.id}.png`, grid.tileData);
-      } else {
-        await prisma.grid.delete({
-          where: { id: grid.id },
-        });
-        logger.error("Tile was supposed to be found in " + map.id);
-      }
+    let iterations = Math.ceil(count / partSize);
+    let iterationPromises = Array.from({ length: iterations }).map((_, i) =>
+      getGridsByIteration(i, map.id, zip)
+    );
 
-      const tmp = await prisma.marker.findMany({
-        where: {
-          gridId: grid.id,
-        },
-      });
-      markers = markers.concat(tmp);
+    let markersData: string[] = [];
+    await parallel(5, await Promise.all(iterationPromises), async (markers) => {
+      markersData = markersData.concat(markers);
     });
-    const gridsWithoutTile = grids.map(({ tileData, ...rest }) => rest);
-    zip.file(`${map.id}/grids.json`, JSON.stringify(gridsWithoutTile));
-    zip.file(`${map.id}/markers.json`, JSON.stringify(markers));
+
+    zip.file(`${map.id}/markers.csv`, markersData.join(";\n"));
   }
   const stream = await zip.generateAsync({ type: "nodebuffer" });
   res.setHeader("Content-Type", "application/zip");
